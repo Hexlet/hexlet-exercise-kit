2551f5997495322f1c9c0bc72f972d3f08111991
Kirill Mokevnin <mokevnin@gmail.com>
2016-03-04 14:54:53 +0300

    init kit

---
+++ README.md
@@ -0,0 +1,17 @@
+[![Hexlet chat](http://slack-ru.hexlet.io/badge.svg)](http://slack-ru.hexlet.io)
+
+# hexlet-exercise-kit
+This is a complete kit for teachers who want to create exercises on the Hexlet platform.
+
+### 1. Get hexlet-exercise-kit
+
+    git clone https://github.com/Hexlet/hexlet-exercise-kit.git
+    cd hexlet-exercise-kit
+
+### 2. Install boot2docker
+
+[boot2docker](http://boot2docker.io/) is a lightweight Linux distribution based on Tiny Core Linux made specifically to run Docker containers. It runs completely from RAM, weighs ~27MB and boots in ~5s.
+
+Go to [http://boot2docker.io/](http://boot2docker.io/) and install the version for your operating system.
+
+Then follow the documentation on [Hexlet Docs](https://github.com/Hexlet/docs/blob/master/create-lesson.md).
---
+++ docker.mk
@@ -0,0 +1,69 @@
+ID := $(shell basename $(CURDIR))
+CONTAINER_ID := $(addsuffix _container, $(ID))
+CONTAINER_ID_INTERNAL := $(addsuffix _container_internal, $(ID))
+IMAGE_ID := $(addsuffix _image, $(ID))
+CS = $(shell docker ps -a -q)
+
+test:
+ifeq ([], $(shell docker inspect $(CONTAINER_ID) 2> /dev/null))
+	@ echo "Please, run 'make start' before 'make test'" >&2; exit 1;
+else
+	docker exec -it $(CONTAINER_ID) /bin/bash -c 'sudo -E PATH=$$PATH -u nobody make test -C /usr/src/app'
+endif
+
+prepare:
+ifeq ([], $(shell docker inspect $(CONTAINER_ID) 2> /dev/null))
+	@ echo "Please, run 'make start' before 'make test'" >&2; exit 1;
+else
+	docker exec -it $(CONTAINER_ID) make prepare -C /usr/src/app
+endif
+
+build: stop
+	docker build -t $(IMAGE_ID) .
+
+bash:
+	docker run -it -v $(CURDIR)/exercise/:/usr/src/app $(IMAGE_ID) /bin/bash -l
+
+attach:
+	docker exec -it $(CONTAINER_ID) /bin/bash -l
+
+logs:
+	docker logs -f $(CONTAINER_ID)
+
+start: stop
+ifeq ([], $(shell docker inspect $(IMAGE_ID) 2> /dev/null))
+	@ echo "Please, run 'make build' before 'make start'" >&2; exit 1;
+else
+	docker run -d -t \
+		-v $(CURDIR)/services.conf:/etc/supervisor/conf.d/services.conf \
+		-v $(CURDIR)/exercise/:/usr/src/app \
+		-v $(CURDIR)/exercise_internal:/exercise_internal \
+		-p 8000:8000 -p 8080:8080 --name $(CONTAINER_ID) $(IMAGE_ID)
+endif
+
+# stop:
+# 	@ docker stop $(CS) > /dev/null 2>&1; echo ""
+
+stop:
+	@ docker rm -v -f $(CS) > /dev/null 2>&1; echo ""
+
+# start_internal: stop
+# ifeq ([], $(shell docker inspect $(IMAGE_ID) 2> /dev/null))
+# 	@ echo "Please, run 'make build'" >&2; exit 1;
+# else
+# 	docker run -d -t -v $(CURDIR)/exercise_internal:/exercise_internal --name $(CONTAINER_ID_INTERNAL) $(IMAGE_ID)
+# endif
+
+test_internal:
+ifeq ([], $(shell docker inspect $(CONTAINER_ID) 2> /dev/null))
+	@ echo "Please, run 'make start_internal'" >&2; exit 1;
+else
+	docker exec $(CONTAINER_ID) make test -C /exercise_internal
+endif
+
+all: build start test
+
+macos:
+	eval "$(docker-machine env default)"
+
+.PHONY: test build bash run stop start
