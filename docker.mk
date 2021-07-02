4beb985c3b3e1b1eb946eca42eebead0f7b8d5c2
Stanislav Dzisiak <stanislav.dzisiak@outlook.com>
2021-07-02 21:11:30 +0300

    fix solution with forwarded envs

--- docker.mk
+++ docker.mk
@@ -5,16 +5,16 @@ CONTAINER_ID_INTERNAL := $(addsuffix _container_internal, $(ID))
 IMAGE_ID := $(addsuffix _image, $(ID))
 CS = $(shell docker ps -a -q)
 ROOT_DIR := $(dir $(abspath $(lastword $(MAKEFILE_LIST))))
-PRESERVE_ENV_LIST := NODE_PATH,PATH
+GET_ENVS := IFS=";" read -r -a ENVS <<< `get-forwarded-envs`
 
 docs-js:
-	docker exec -it $(CONTAINER_ID) /bin/bash -c 'sudo --preserve-env=${PRESERVE_ENV_LIST} -u $(USER) rm -rf docs && mkdir -p docs && /import-documentation/dist/bin/import-documentation.js . -o docs'
+	docker exec -it $(CONTAINER_ID) /bin/bash -c '$(GET_ENVS) && sudo "$${ENVS[@]}" -u $(USER) rm -rf docs && mkdir -p docs && /import-documentation/dist/bin/import-documentation.js . -o docs'
 
 test:
 ifeq ([], $(shell docker inspect $(CONTAINER_ID) 2> /dev/null))
 	@ echo "Please, run 'make start' before 'make test'" >&2; exit 1;
 else
-	docker exec -it $(CONTAINER_ID) /bin/bash -c 'sudo --preserve-env=${PRESERVE_ENV_LIST} -u $(USER) make test'
+	docker exec -it $(CONTAINER_ID) /bin/bash -c '$(GET_ENVS) && sudo "$${ENVS[@]}" -u $(USER) make test'
 endif
 
 prepare:
@@ -29,18 +29,20 @@ build: stop
 
 bash:
 	docker run --read-only -it -v /tmp \
+		-v $(ROOT_DIR)scripts/get-forwarded-envs:/usr/local/bin/get-forwarded-envs \
 	  -v $(CURDIR)/exercise_internal:/exercise_internal \
 	  -v $(CURDIR)/exercise/:/usr/src/app $(IMAGE_ID) \
-	  /bin/bash -c 'sudo -u $(USER) --preserve-env=${PRESERVE_ENV_LIST} -s'
+	  /bin/bash -c '$(GET_ENVS) && sudo "$${ENVS[@]}" -u $(USER) -s'
 
 bash-root:
 	docker run -it -v /tmp \
+		-v $(ROOT_DIR)scripts/get-forwarded-envs:/usr/local/bin/get-forwarded-envs \
 	  -v $(CURDIR)/exercise_internal:/exercise_internal \
 	  -v $(CURDIR)/exercise/:/usr/src/app $(IMAGE_ID) \
-	  /bin/bash
+	  /bin/bash -c '$(GET_ENVS) && sudo "$${ENVS[@]}" -u root -s'
 
-attach:
-	docker exec -it $(CONTAINER_ID) /bin/bash -c 'sudo --preserve-env=${PRESERVE_ENV_LIST} -u $(USER) -s'
+attach: start
+	docker exec -it $(CONTAINER_ID) /bin/bash -c '$(GET_ENVS) && sudo "$${ENVS[@]}" -u $(USER) -s'
 
 logs:
 	docker logs -f $(CONTAINER_ID)
@@ -53,7 +55,7 @@ else
 		--label hexlet-exercise \
 		-v $(ROOT_DIR)import-documentation:/import-documentation \
 		-v /tmp \
-		-e $(shell $(ROOT_DIR)scripts/forward-envs) \
+		-v $(ROOT_DIR)scripts/get-forwarded-envs:/usr/local/bin/get-forwarded-envs \
 		-v $(CURDIR)/services.conf:/etc/supervisor/conf.d/services.conf \
 		-v $(CURDIR)/exercise/:/usr/src/app \
 		-v $(CURDIR)/exercise_internal:/exercise_internal \
@@ -78,7 +80,7 @@ ifeq ([], $(shell docker inspect $(CONTAINER_ID) 2> /dev/null))
 	@ echo "Please, run 'make start_internal'" >&2; exit 1;
 else
 	# docker exec $(CONTAINER_ID) make test -C /exercise_internal
-	docker exec $(CONTAINER_ID) /bin/bash -c 'sudo --preserve-env=${PRESERVE_ENV_LIST} -u $(USER) make test -C /exercise_internal'
+	docker exec $(CONTAINER_ID) /bin/bash -c '$(GET_ENVS) && sudo "$${ENVS[@]}" -u $(USER) make test -C /exercise_internal'
 endif
 
 lint-js:
