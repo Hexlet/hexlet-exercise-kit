d5c4a96dbc7d5792c959213fcfffaad43c3ef790
feycot <feycot@gmail.com>
2022-10-21 02:11:15 +0500

    fix

--- repo-downloader/lib/repo_downloader/cli.rb
+++ repo-downloader/lib/repo_downloader/cli.rb
@@ -12,13 +12,20 @@ module RepoDownloader
         raise 'You should to add Gitlab private token in .env file'
       end
 
+      repos_path =
+        if ENV['PATH_TO_REPOS'].nil?
+          File.expand_path('..', Dir.getwd)
+        else
+          File.expand_path(ENV['PATH_TO_REPOS'])
+        end
+
       options = {
         parallel: ENV['PARALLEL'].to_i || 8,
         filter: ENV.fetch('FILTER', nil),
         update: ENV.fetch('UPDATE', nil) == 'true',
         gitlab_endpoint: ENV.fetch('GITLAB_API_ENDPOINT', nil),
         gitlab_private_token: ENV.fetch('GITLAB_API_PRIVATE_TOKEN', nil),
-        repos_path: File.expand_path('..', Dir.getwd)
+        repos_path: repos_path
       }
 
       downloader = RepoDownloader::Downloader.new(options)
