a8d4b32787e4927618e80137af1cf2bc3c93e4cf
Stanislav Dzisiak <stanislav.dzisiak@gmail.com>
2024-08-22 10:44:40 +0300

    do not use supervisor

--- docker.mk
+++ docker.mk
@@ -70,7 +70,6 @@ else
 		-v /tmp \
 		-v /var/tmp \
 		-v $(ROOT_DIR)scripts/get-forwarded-envs:/usr/local/bin/get-forwarded-envs \
-		-v $(CURDIR)/services.conf:/etc/supervisor/conf.d/services.conf \
 		-v $(CURDIR)/exercise/:/usr/src/app \
 		-v $(CURDIR)/exercise_internal:/exercise_internal \
 		-p 8000:8000 -p 80:8080 --name $(CONTAINER_ID) $(IMAGE_ID)
